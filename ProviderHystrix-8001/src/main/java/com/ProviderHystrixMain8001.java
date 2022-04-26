5830e5379a35ddda424ae9508678efd683f7416e
magidragon <magidragon@foxmail.com>
2022-04-26 17:18:56 +0800

    [INIT]Provider服务提供工程构建

---
+++ ProviderHystrix-8001/src/main/java/com/ProviderHystrixMain8001.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class ProviderHystrixMain8001 {
+}
