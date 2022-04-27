43dc533f99e6d26f1a6becc4b49396211961c345
magidragon <magidragon@foxmail.com>
2022-04-27 10:36:22 +0800

    [INIT]Order Controller

--- ProviderHystrixOrder-8003/src/main/java/com/controller/OrderProviderController.java
+++ ProviderHystrixOrder-8003/src/main/java/com/controller/OrderProviderController.java
@@ -1,9 +1,10 @@
 package com.controller;
 
+import com.CommonResult;
+import com.entities.Order;
 import com.service.OrderProviderService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -14,5 +15,36 @@ public class OrderProviderController {
     @Resource
     private OrderProviderService orderProviderService;
 
+    @RequestMapping("/createOrder")
+    public CommonResult createOrder(@RequestBody Order order){
+        int result = orderProviderService.createOrder(order);
+        log.info("*****插入结果:"+ result);
+        if(result > 0){
+            return new CommonResult(200,"插入数据成功。", result);
+        }else{
+            return new CommonResult(400,"插入数据失败。", null);
+        }
+    }
 
+    @GetMapping(value = "/getOrder/{id}")
+    public CommonResult getOrder(@PathVariable("id") Long id){
+        Order order = orderProviderService.getOrder(id);
+        log.info("*****查询结果：{order}");
+        if(order != null){
+            return new CommonResult(200,"查询数据成功。", order);
+        }else{
+            return new CommonResult(400,"查询数据失败。", null);
+        }
+    }
+
+    @GetMapping("/deleteOrder/{id}")
+    public CommonResult deleteOrder(@PathVariable("id") Long id){
+        int result = orderProviderService.deleteOrder(id);
+        log.info("*****删除结果：{result}");
+        if(result > 0){
+            return new CommonResult(200,"删除数据成功。", result);
+        }else {
+            return new CommonResult(400,"删除数据失败。", result);
+        }
+    }
 }
