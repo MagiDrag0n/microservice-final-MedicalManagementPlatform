2acadeb2505424ff71c2bd754b1d4d1326f7c722
magidragon <magidragon@foxmail.com>
2022-04-27 10:26:30 +0800

    [INIT]OrderMain.java

---
+++ ProviderHystrixOrder-8003/src/main/java/com/OrderMain.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class OrderMain {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain.class, args);
+    }
+}
