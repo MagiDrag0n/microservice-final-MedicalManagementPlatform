915a9d1e15c14c7ec8a78704f9020d5651a62296
magidragon <magidragon@foxmail.com>
2022-04-28 00:04:19 +0800

    [UPDATE]

--- API-commons/src/main/java/com/entities/Doctor.java
+++ API-commons/src/main/java/com/entities/Doctor.java
@@ -11,6 +11,6 @@ public class Doctor {
     private Long id;//主键，医生编号唯一
     private String name;//医生名称
     private String title;//医生职称
-    private Long DeptId;//医生科室
-    private String validTime;//工作时间
+    private Long deptid;//医生科室
+    private String validtime;//工作时间
 }
