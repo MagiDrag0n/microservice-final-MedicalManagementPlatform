ee900ce444f84dc4cdb2353df0a31bc384b325f4
magidragon <magidragon@foxmail.com>
2022-04-27 12:41:07 +0800

    [UPDATE]

--- API-commons/src/main/java/com/entities/Order.java
+++ API-commons/src/main/java/com/entities/Order.java
@@ -13,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Order {
     private Long id;//主键，订单编号唯一
-    private Long PatientId;//患者id
+    private Long patientid;//患者id
     private BigDecimal bill;//订单总价
     private Timestamp time;//订单时间
     private int valid;//表示当前订单有效情况
