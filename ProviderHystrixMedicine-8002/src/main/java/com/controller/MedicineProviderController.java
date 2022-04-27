485acc37ae5fe74d95d71cdcb7ea1b059a907b13
magidragon <magidragon@foxmail.com>
2022-04-27 10:02:45 +0800

    [INIT]Medicine Controller Logger

--- ProviderHystrixMedicine-8002/src/main/java/com/controller/MedicineProviderController.java
+++ ProviderHystrixMedicine-8002/src/main/java/com/controller/MedicineProviderController.java
@@ -3,12 +3,14 @@ package com.controller;
 import com.CommonResult;
 import com.entities.Medicine;
 import com.service.MedicineProviderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("medicine")
+@Slf4j
 public class MedicineProviderController {
     @Resource
     private MedicineProviderService medicineProviderService;
@@ -16,7 +18,7 @@ public class MedicineProviderController {
     @RequestMapping("/addMedicine")
     public CommonResult addMedicine(@RequestBody Medicine medicine){
         int result = medicineProviderService.addMedicine(medicine);
-//        log.info("*****插入结果:"+ result);
+        log.info("*****插入结果:"+ result);
         if(result > 0){
             return new CommonResult(200,"插入数据成功。", result);
         }else{
@@ -27,7 +29,7 @@ public class MedicineProviderController {
     @GetMapping(value = "/getMedicine/{id}")
     public CommonResult getMedicine(@PathVariable("id") Long id){
         Medicine medicine = medicineProviderService.getMedicine(id);
-//        log.info("*****插入结果：{medicine}");
+        log.info("*****查询结果：{medicine}");
         if(medicine != null){
             return new CommonResult(200,"查询数据成功。", medicine);
         }else{
@@ -38,6 +40,7 @@ public class MedicineProviderController {
     @RequestMapping(value = "/updateMedicine")
     public CommonResult updateMedicine(@RequestBody Medicine medicine){
         int result = medicineProviderService.updateMedicine(medicine);
+        log.info("*****更新结果：{result}");
         if(result > 0){
             return new CommonResult(200,"更新数据成功。", result);
         }else {
@@ -48,6 +51,7 @@ public class MedicineProviderController {
     @GetMapping("/deleteMedicine/{id}")
     public CommonResult deleteMedicine(@PathVariable("id") Long id){
         int result = medicineProviderService.deleteMedicine(id);
+        log.info("*****删除结果：{result}");
         if(result > 0){
             return new CommonResult(200,"删除数据成功。", result);
         }else {
