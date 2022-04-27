9401c5185fe9fc86d865d73f1b5d519605c00e46
magidragon <magidragon@foxmail.com>
2022-04-27 23:30:49 +0800

    [INIT]Dept DAO

--- ProviderHystrixDept-8004/src/main/java/com/dao/DeptProviderDAO.java
+++ ProviderHystrixDept-8004/src/main/java/com/dao/DeptProviderDAO.java
@@ -6,6 +6,11 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface DeptProviderDAO {
+    public int initDept(Dept dept);
+    public Dept getDept(@Param("id") Long id);
+    public int updateDept(Dept dept);
+//    public int deleteDept(@Param("id") Long id);
+//    医院大概率不会取消某个科室吧？
 
 
 }
