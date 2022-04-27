a8f80b9f3e735122ecda0098aa7f6589e2ed9ea9
magidragon <magidragon@foxmail.com>
2022-04-27 10:18:53 +0800

    [INIT]Order DAO Mapper

--- ProviderHystrixUser-8001/src/main/java/com/dao/UserProviderDAO.java
+++ ProviderHystrixUser-8001/src/main/java/com/dao/UserProviderDAO.java
@@ -9,12 +9,6 @@ public interface UserProviderDAO {
     public User getUser(@Param("id") Long id);
     public int updateUser(User user);
 
-    public int createOrder(Order order);
-    public Order getOrder(@Param("id") Long id);
-//    public int updateOrder(Order order);
-//    开药订单不应修改，确实要修改则作废重新开单。
-    public int deleteOrder(@Param("id") Long id);
-
     public int initDept(Dept dept);
     public Dept getDept(@Param("id") Long id);
     public int updateDept(Dept dept);
