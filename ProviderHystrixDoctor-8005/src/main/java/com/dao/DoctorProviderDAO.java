fe4850f889a9605ac4c5d2451ab3e4d7dfdd8065
magidragon <magidragon@foxmail.com>
2022-04-27 23:59:58 +0800

    [INIT]Doctor DAO

--- ProviderHystrixDoctor-8005/src/main/java/com/dao/DoctorProviderDAO.java
+++ ProviderHystrixDoctor-8005/src/main/java/com/dao/DoctorProviderDAO.java
@@ -1,4 +1,13 @@
 package com.dao;
 
+import com.entities.Doctor;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface DoctorProviderDAO {
+    public int empDoctor(Doctor doctor);
+    public Doctor getDoctor(@Param("id") Long id);
+    public int updateDoctor(Doctor doctor);
+    public int ueDoctor(@Param("id") Long id);
 }
