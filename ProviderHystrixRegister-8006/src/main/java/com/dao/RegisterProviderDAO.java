ef9db66b704b79b755016bf96838719cc7778739
magidragon <magidragon@foxmail.com>
2022-04-30 23:00:46 +0800

    [UPDATE]Register DAO Mapper

--- ProviderHystrixRegister-8006/src/main/java/com/dao/RegisterProviderDAO.java
+++ ProviderHystrixRegister-8006/src/main/java/com/dao/RegisterProviderDAO.java
@@ -1,4 +1,12 @@
 package com.dao;
 
+import com.entities.Register;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
 public interface RegisterProviderDAO {
+    public int signRegister(Register register);
+    public Register getRegister(@Param("id") Long id);
+//    挂号当日有效，过期无效。换科室、医生只需要重新叫好即可。
 }
