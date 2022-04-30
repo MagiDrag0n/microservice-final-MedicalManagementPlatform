5f873df375db5ae9b6676784b958248b98f89b75
magidragon <magidragon@foxmail.com>
2022-04-30 23:29:21 +0800

    [UPDATE]application 时区、时间格式化

--- API-commons/src/main/java/com/entities/Register.java
+++ API-commons/src/main/java/com/entities/Register.java
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class Register {
     private Long id;//主键，挂号编号唯一
-    private Long PatientId;//患者编号
-    private Long DoctorId;//医生编号
+    private Long patientid;//患者编号
+    private Long doctorid;//医生编号
     private Timestamp timestamp;//挂号时间
 }
