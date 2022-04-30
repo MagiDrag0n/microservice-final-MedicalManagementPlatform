fd81cdde4fee0186b2e67d4e70e641189b5661ab
magidragon <magidragon@foxmail.com>
2022-04-30 23:00:22 +0800

    [UPDATE]

--- ProviderHystrixUser-8001/src/main/java/com/dao/UserProviderDAO.java
+++ ProviderHystrixUser-8001/src/main/java/com/dao/UserProviderDAO.java
@@ -8,8 +8,4 @@ public interface UserProviderDAO {
     public int regisUser(User user);
     public User getUser(@Param("id") Long id);
     public int updateUser(User user);
-
-    public int signRegister(Register register);
-    public Register getRegister(@Param("id") Long id);
-//    挂号当日有效，过期无效。换科室、医生只需要重新叫好即可。
 }
