b198d1f05ef9307274650ae395e548c0db94ff54
magidragon <magidragon@foxmail.com>
2022-04-30 23:14:46 +0800

    [UPDATE]Register Controller

--- ProviderHystrixRegister-8006/src/main/java/com/controller/RegisterProviderController.java
+++ ProviderHystrixRegister-8006/src/main/java/com/controller/RegisterProviderController.java
@@ -1,9 +1,10 @@
 package com.controller;
 
+import com.CommonResult;
+import com.entities.Register;
 import com.service.RegisterProviderService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -13,4 +14,26 @@ import javax.annotation.Resource;
 public class RegisterProviderController {
     @Resource
     private RegisterProviderService registerProviderService;
+
+    @RequestMapping(value = "/signRegister")
+    public CommonResult signRegister(@RequestBody Register register){
+        int result = registerProviderService.signRegister(register);
+        log.info("*****插入结果:"+result);
+        if(result > 0) {
+            return new CommonResult(200,"插入数据成功。", result);
+        }else{
+            return new CommonResult(400,"插入数据失败", null);
+        }
+    }
+
+    @GetMapping(value = "/getRegister/{id}")
+    public CommonResult getRegister(@PathVariable("id") Long id){
+        Register register = registerProviderService.getRegister(id);
+        log.info("*****查询结果：{register}");
+        if (register != null) {
+            return new CommonResult(200,"查询成功", register);
+        } else {
+            return new CommonResult(400, "无记录", null);
+        }
+    }
 }
