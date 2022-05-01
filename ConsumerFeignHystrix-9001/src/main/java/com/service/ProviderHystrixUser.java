95c016a150773e3b13433afb9efc5abe62cdcbe5
magidragon <magidragon@foxmail.com>
2022-05-01 17:21:38 +0800

    [UPDATE]

--- ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixUser.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixUser.java
@@ -4,13 +4,12 @@ import com.CommonResult;
 import com.entities.User;
 import com.service.fallback.ProviderHystrixUserFallback;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "PROVIDER-HYSTRIX-USER-8001",contextId = "User")
+@FeignClient(value = "PROVIDER-HYSTRIX-USER-8001", fallback = ProviderHystrixUserFallback.class)
 public interface ProviderHystrixUser {
     @RequestMapping(value = "/user/regisUser")
     CommonResult regisUser_OK(@RequestBody User user);
--- ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
@@ -3,21 +3,21 @@ package com.service.fallback;
 import com.CommonResult;
 import com.entities.User;
 import com.service.ProviderHystrixUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ProviderHystrixUserFallback implements ProviderHystrixUser {
     @Override
     public CommonResult regisUser_OK(User user) {
-        return null;
+        log.info("*****触发服务降级。");
+        return new CommonResult(400,"服务正忙，请稍后再试。",null);
     }
 
     @Override
     public CommonResult getUser_OK(Long id) {
-        return null;
-    }
-
-    public String GlobalFallback(){
-        return "服务正忙，请稍后再试。";
+        log.info("*****触发服务降级。");
+        return new CommonResult(400,"服务正忙，请稍后再试。",null);
     }
 }
