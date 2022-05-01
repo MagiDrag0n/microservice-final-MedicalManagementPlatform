53b80e190536845cb326f91345bc76152a400811
magidragon <magidragon@foxmail.com>
2022-05-01 20:07:54 +0800

    [UPDATE]Dept 模块实现服务降级

--- ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixDept.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixDept.java
@@ -1,9 +1,22 @@
 package com.service;
 
+import com.CommonResult;
+import com.entities.Dept;
 import com.service.fallback.ProviderHystrixDeptFallback;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(value = "PROVIDER-HYSTRIX-DEPT-8004", fallback = ProviderHystrixDeptFallback.class)
 public interface ProviderHystrixDept {
-    @
+    @RequestMapping(value = "/dept/initDept")
+    CommonResult initDept(@RequestBody Dept dept);
+
+    @GetMapping(value = "/dept/getDept/{id}")
+    CommonResult getDept(@PathVariable("id") Long id);
+
+    @RequestMapping(value = "/dept/updateDept")
+    CommonResult updateDept(@RequestBody Dept dept);
 }
--- ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixDeptFallback.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixDeptFallback.java
@@ -1,6 +1,22 @@
 package com.service.fallback;
 
+import com.CommonResult;
+import com.entities.Dept;
 import com.service.ProviderHystrixDept;
 
 public class ProviderHystrixDeptFallback implements ProviderHystrixDept {
+    @Override
+    public CommonResult initDept(Dept dept) {
+        return new CommonResult(400,"服务正忙，请稍后再试。",null);
+    }
+
+    @Override
+    public CommonResult getDept(Long id) {
+        return new CommonResult(400,"服务正忙，请稍后再试。",null);
+    }
+
+    @Override
+    public CommonResult updateDept(Dept dept) {
+        return new CommonResult(400,"服务正忙，请稍后再试。",null);
+    }
 }
