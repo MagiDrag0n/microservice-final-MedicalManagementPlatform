e31e0d34afecedb30a1460503c3ed9ba4f41d009
magidragon <magidragon@foxmail.com>
2022-05-01 16:01:08 +0800

    [UPDATE]业务降级

--- ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
@@ -1,6 +1,19 @@
 package com.service.fallback;
 
+import com.CommonResult;
+import com.entities.User;
 import com.service.ProviderHystrixUser;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProviderHystrixUserFallback implements ProviderHystrixUser {
+    @Override
+    public CommonResult regisUser_OK(User user) {
+        return null;
+    }
+
+    @Override
+    public CommonResult getUser_OK(Long id) {
+        return null;
+    }
 }
