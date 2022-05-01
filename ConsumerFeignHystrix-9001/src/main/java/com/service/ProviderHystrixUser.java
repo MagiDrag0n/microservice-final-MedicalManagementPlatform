858cac8b12dcd51aa0b35f328e59ab4608a47601
magidragon <magidragon@foxmail.com>
2022-05-01 15:46:04 +0800

    [UPDATE]Consumer 服务熔断、降级

---
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixUser.java
@@ -0,0 +1,21 @@
+package com.service;
+
+import com.CommonResult;
+import com.entities.User;
+import com.service.fallback.ProviderHystrixUserFallback;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@FeignClient(value = "provider-hystrix-user-8001", fallback = ProviderHystrixUserFallback.class)
+public interface ProviderHystrixUser {
+    @RequestMapping(value = "/user/regisUser")
+    CommonResult regisUser_OK(@RequestBody User user);
+
+    @GetMapping(value = "/user/getUser/{id}")
+    CommonResult getUser_OK(@PathVariable("id") Long id);
+}
---
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
@@ -0,0 +1,6 @@
+package com.service.fallback;
+
+import com.service.ProviderHystrixUser;
+
+public class ProviderHystrixUserFallback implements ProviderHystrixUser {
+}
