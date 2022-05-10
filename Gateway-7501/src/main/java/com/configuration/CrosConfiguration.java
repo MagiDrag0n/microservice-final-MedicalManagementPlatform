0313160e3b46fd67e2031165f2eb17dcbb7bb574
magidragon <magidragon@foxmail.com>
2022-05-10 21:55:14 +0800

    [UPDATE]跨域支持

--- Gateway-7501/src/main/java/com/configuration/CrosConfiguration.java
+++ Gateway-7501/src/main/java/com/configuration/CrosConfiguration.java
@@ -1,7 +1,4 @@
 package com.configuration;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class CrosConfiguration implements WebMvcConfigurer {
+public class CrosConfiguration {
 }
