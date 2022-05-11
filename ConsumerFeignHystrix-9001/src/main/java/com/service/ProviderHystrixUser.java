25e4825d63c1cc21e9f31f6a98b4c4ee64c4a0e4
magidragon <magidragon@foxmail.com>
2022-05-11 21:19:53 +0800

    [UPDATE]user Feign客户端接入网关

--- ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixUser.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/ProviderHystrixUser.java
@@ -4,10 +4,9 @@ import com.CommonResult;
 import com.entities.User;
 import com.service.fallback.ProviderHystrixUserFallback;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @FeignClient(value = "PROVIDER-HYSTRIX-USER-8001", fallback = ProviderHystrixUserFallback.class)
 public interface ProviderHystrixUser {
@@ -19,5 +18,11 @@ public interface ProviderHystrixUser {
 
     @RequestMapping(value = "/user/updateUser")
     CommonResult updateUser(@RequestBody User user);
+
+    @PostMapping(value = "/user/login")
+    CommonResult login(@RequestBody User user);
+
+    @PostMapping(value = "/user/checkToken")
+    Boolean checkToken(@RequestHeader(value = "token") String token);
 
 }
