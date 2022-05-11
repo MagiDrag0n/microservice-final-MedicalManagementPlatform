4f1a43b637a8db6092bd79e88619c1b228ff0a56
magidragon <magidragon@foxmail.com>
2022-05-11 21:20:09 +0800

    [UPDATE]JWT的服务降级熔断方法

--- ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/service/fallback/ProviderHystrixUserFallback.java
@@ -5,6 +5,9 @@ import com.entities.User;
 import com.service.ProviderHystrixUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Component
 @Slf4j
@@ -25,5 +28,18 @@ public class ProviderHystrixUserFallback implements ProviderHystrixUser {
     public CommonResult updateUser(User user) {
         log.info("*****触发服务降级。");
         return new CommonResult(400,"服务正忙，请稍后再试。",null);
+    }
+
+    @Override
+    public CommonResult login(User user) {
+        log.info(String.valueOf(user));
+        log.info("*****触发服务降级。");
+        return new CommonResult(400,"服务正忙，请稍后再试。",null);
+    }
+
+    @Override
+    public Boolean checkToken(@RequestHeader(value = "token") String token) {
+        log.info("*****触发服务降级。");
+        return false;
     }
 }
