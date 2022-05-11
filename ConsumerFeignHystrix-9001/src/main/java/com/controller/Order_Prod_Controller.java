fa45b1fa6b75779eef74b9abe97b39356b90b485
magidragon <magidragon@foxmail.com>
2022-05-11 21:19:42 +0800

    [UPDATE]order Feign客户端接入网关

--- ConsumerFeignHystrix-9001/src/main/java/com/controller/Order_Prod_Controller.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/controller/Order_Prod_Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping(value = "me")
+@RequestMapping(value = "order")
 @Slf4j
 @DefaultProperties(defaultFallback = "ProviderHystrixOrderFallback", commandProperties = {
         @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  // 开启熔断器
