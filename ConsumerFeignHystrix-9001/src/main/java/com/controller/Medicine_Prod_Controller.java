3670fa2cf26ccb34fa3f29512199fb7eaed3dff5
magidragon <magidragon@foxmail.com>
2022-05-11 21:19:35 +0800

    [UPDATE]medicine Feign客户端接入网关

--- ConsumerFeignHystrix-9001/src/main/java/com/controller/Medicine_Prod_Controller.java
+++ ConsumerFeignHystrix-9001/src/main/java/com/controller/Medicine_Prod_Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping(value = "me")
+@RequestMapping(value = "medicine")
 @Slf4j
 @DefaultProperties(defaultFallback = "ProviderHystrixMedicineFallback", commandProperties = {
         @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  // 开启熔断器
